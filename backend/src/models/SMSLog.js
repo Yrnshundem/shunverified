c62733e41439ce390b0ce8a258aa0b00ee145816
Yrnshundem <anitaleele53@gmail.com>
2025-09-06 05:55:40 +0000

    Update SMSLog.js

--- backend/src/models/SMSLog.js
+++ backend/src/models/SMSLog.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+lconst mongoose = require('mongoose');
 
 const SMSLogSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   phoneNumber: { type: String, required: true },
   service: { type: String, required: true },
   message: { type: String, required: true },
+  activationId: { type: String }, // Link to Number model
+  isDelivered: { type: Boolean, default: false },
   receivedAt: { type: Date, default: Date.now }
 });
 
