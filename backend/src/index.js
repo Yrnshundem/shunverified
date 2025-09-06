a4d165b679fd7ff3f65608a2d6825f5741760eb4
Yrnshundem <anitaleele53@gmail.com>
2025-09-06 07:19:45 +0000

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,9 +22,9 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => console.error('MongoDB connection error:', err.message));
 
 app.use('/api/auth', authRouter);
 app.use('/api/numbers', numbersRouter);
